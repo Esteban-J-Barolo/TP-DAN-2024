d6bce0357ea2a8e7d2ed174d682a891ed939a3d0
Esteban-J-Barolo <estebanjavierbarolo@gmial.com>
2024-10-21 12:04:42 -0300

    first commit

---
+++ ms-clientes/clientes/src/test/java/dan/barolo/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.barolo.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
